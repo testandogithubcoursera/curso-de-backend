dd8eaf6f79c54a12bf23da78e597cbdb9285d9f5
matheus <matheu.play8@hotmail.com>
2022-06-03 16:01:57 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var rect = require('./rectangle')
+
+function solveRect(l,b){
+    console.log("solving for rectangle with l =");
+
+    if(l <=0 || b <=0){
+        console.log("rectangle dimension should be greather than 0")
+    }
+    else{
+        console.log("the area of the rectangle is " + l * b);
+        console.log("the perimiter is");
+    }
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
